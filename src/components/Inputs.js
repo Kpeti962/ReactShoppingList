544ab77debef0613635e3b4f89382a535aa1bb07
Peti <kpeti962@gmailcom>
2022-07-25 11:14:01 +0200

    use some icons

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -2,6 +2,10 @@ import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
 import List from "./List";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+ faPlus
+} from "@fortawesome/free-solid-svg-icons";
 
 const Inputs = ({
   newItem,
@@ -25,14 +29,14 @@ const Inputs = ({
 
   const addToList = (e) => {
     e.preventDefault();
-  
-    if(foodInput === ""){
-    alert("Please type the product")
-     } else if (qntityInput === ""){
-      alert("Please type the quantity")
-     } else if(selectInput === "") {
-      alert("Please add the unit")
-     } else {
+
+    if (foodInput === "") {
+      alert("Please type the product");
+    } else if (qntityInput === "") {
+      alert("Please type the quantity");
+    } else if (selectInput === "") {
+      alert("Please add the unit");
+    } else {
       setNewItem([
         ...newItem,
         {
@@ -40,51 +44,63 @@ const Inputs = ({
           quantity: qntityInput,
           unit: selectInput,
           id: uuidv4(),
-          purchase: false
+          purchase: false,
         },
       ]);
       setFoodInput("");
       setQntityInput("");
       setSelectInput("");
-     }
+    }
+  };
+
+  const inputStyle = {
+    background: "rgb(207, 170, 3)",
+    border: "none",
+    
   };
 
   return (
     <div className="inputsAndList">
-      <div className="inputs-btns">
+        <div className="inputs-btns">
         <input
-          placeholder="Product"
+          style={inputStyle}
           type="text"
-          className="input input-text"
+          className="input inputText"
+          id="inp"
+          placeholder="Termék"
           value={foodInput}
           onChange={foodInputHandler}
-          
         />
         <input
-          placeholder="Quantity"
+          style={inputStyle}
+          placeholder="Mennyiség"
           type="number"
           className="quantityinput"
           value={qntityInput}
           onChange={qntityInputHandler}
         />
         <select
+          style={inputStyle}
+          className="selectInput"
           name=""
           id="selectInput"
           value={selectInput}
           onChange={selectInputHandler}
         >
-          <option value="default">Choose</option>
-          <option value="pack">pack</option>
-          <option value="piece">piece</option>
+          <option value="default">Válassz</option>
+          <option value="pack">Csomag</option>
+          <option value="piece">db</option>
           <option value="kg">kg</option>
           <option value="dkg">dkg</option>
           <option value="g">g</option>
           <option value="l">l</option>
         </select>
         <button className="btn" onClick={addToList}>
-          Add item
+        <FontAwesomeIcon icon={faPlus} />
         </button>
       </div>
+      
+      
 
       <div className="list">
         <ul id="list">
@@ -100,7 +116,6 @@ const Inputs = ({
                 id={id}
                 item={item}
                 purchase={purchase}
-                
               />
             );
           })}
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartShopping,
+  faTrash,
+  faRotateLeft,
+} from "@fortawesome/free-solid-svg-icons";
 
-const List = ({ productName, quantity, unit, newItem, setNewItem, id, item, purchase }) => {
+const List = ({
+  productName,
+  quantity,
+  unit,
+  newItem,
+  setNewItem,
+  id,
+  item,
+  purchase,
+}) => {
   const deleteHandler = (clickedItemId) => {
     console.log(clickedItemId);
     const filteredArray = newItem.filter((item) => item.id !== clickedItemId);
@@ -11,17 +26,26 @@ const List = ({ productName, quantity, unit, newItem, setNewItem, id, item, purc
   const doneHandler = (rowId) => {
     let changedArray = newItem;
     changedArray.forEach((clickedRow) => {
-      if (clickedRow.id === rowId){
-        clickedRow.purchase = true
+      if (clickedRow.id === rowId) {
+        clickedRow.purchase = true;
       }
-    })
+    });
     console.log(changedArray);
-    setNewItem([...changedArray])
-  }
+    setNewItem([...changedArray]);
+  };
 
+  const resetHandler = (rowId) => {
+    let changedArray = newItem;
+    changedArray.forEach((clickedRow) => {
+      if (clickedRow.id === rowId) {
+        clickedRow.purchase = false;
+      }
+    });
+    console.log(changedArray);
+    setNewItem([...changedArray]);
+  };
 
-
-/* const doneHandler = (rowId) => {
+  /* const doneHandler = (rowId) => {
   const filteredArray = newItem.filter((item) => item.id === rowId);
   console.log(filteredArray);
   const newItemsWithoutFiltered = newItem.filter((item) => item.id !== rowId);
@@ -31,48 +55,42 @@ console.log(clickedRow);
 setNewItem([...newItemsWithoutFiltered, clickedRow])
 }  */
 
+  const purchasedItemStyle = {
+    color: "rgb(21, 126, 0)",
+    textDecoration: "line-through",
+  };
 
-const purchasedItemStyle = {
-  color: "rgb(21, 126, 0)",
-  textDecoration: 'line-through'
-}
-
-const notPurchasedItemStyle = {
-  color: "black"
-}
-/* e.target.style.setProperty('background', 'lightgreen')
-e.target.style.setProperty('border', '0 solid black')   */
-
-
-
-/* const mapped = newItem.map((item) => {
-  if (item.id === rowId)
-  {mapped.target.style.setProperty('background', 'lightgreen')}
-  console.log(rowId);
-
-}
-) */
-
+  const notPurchasedItemStyle = {
+    color: "black",
+  };
 
   return (
-      <li>
-    <div style={purchase ? purchasedItemStyle : notPurchasedItemStyle}className="inputList" contentEditable="true" onChange={doneHandler}>
-      {productName} / {quantity} {unit} 
-    </div>
-    <div className="deleteAndCheckedBtns">
-      {purchase === false && 
-      <button onClick={() => doneHandler(id)}>Purchased</button>
-      }
-      {purchase === true && 
-      <button>Visszaállít</button>
-      }
-      <button onClick={() => deleteHandler(id)}>Delete</button>
-
-    </div>
+    <li>
+      <div
+        style={purchase ? purchasedItemStyle : notPurchasedItemStyle}
+        className="inputList"
+        contentEditable="true"
+        onChange={doneHandler}
+      >
+        {productName} - {quantity} {unit}
+      </div>
+      <div className="deleteAndCheckedBtns">
+        {purchase === false && (
+          <button onClick={() => doneHandler(id)}>
+            <FontAwesomeIcon icon={faCartShopping} className="iconCart" />
+          </button>
+        )}
+        {purchase === true && (
+          <button onClick={() => resetHandler(id)}>
+            <FontAwesomeIcon icon={faRotateLeft} className="iconBack" />
+          </button>
+        )}
+        <button onClick={() => deleteHandler(id)}>
+          <FontAwesomeIcon icon={faTrash} className="iconRed" />
+        </button>
+      </div>
     </li>
   );
 };
 
 export default List;
-
-
